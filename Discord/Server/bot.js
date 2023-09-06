0ca7bf53d937bbcf37ef70d2f8f9333ff72c65d6
Bryant Trinh <trinhbryant@gmail.com>
2023-09-06 14:03:35 -0700

    added more bot logic

--- Discord/Server/bot.js
+++ Discord/Server/bot.js
@@ -7,6 +7,14 @@ const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
 const TOKEN = process.env.BOT_TOKEN;
 
 
-console.log(process.env.BOT_TOKEN);
+console.log('Bot is online');
 
 client.login(TOKEN);
+
+client.on("message", message => {
+  if (message.content.toLowerCase() == "k.shutdown"){
+    message.channel.send("KaraOkay has left the building T--T").then(() => {
+      client.destroy();
+    })
+  }
+})
