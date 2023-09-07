a5333478ff4f9c1418ff17bb386a9b4d45bf9ddd
Bryant Trinh <trinhbryant@gmail.com>
2023-09-07 01:07:27 -0700

    updated bot token

---
+++ src/Discord/Server/config/index.cjs
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`KaraOKAY has entered the building! \u{1F3A4}\u{1F9D1}`);
+});
+
+client.login(process.env.BOT_TOKEN);
